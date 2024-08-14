2f927344b34a765df76beceec63d92cecddcc27b
ASUS <ASUS@DESKTOP-79FQ8RI>
2024-08-14 09:56:15 +0530

    commit 2

--- C43_Thamaraikannan_S/src/com/tnsif/day2/KeywordsPrimitiveLooping/ControlStatement.java
+++ C43_Thamaraikannan_S/src/com/tnsif/day2/KeywordsPrimitiveLooping/ControlStatement.java
@@ -3,12 +3,25 @@ package com.tnsif.day2.KeywordsPrimitiveLooping;
 public class ControlStatement {
 
 	public static void main(String[] args) {
-		int i=99;
-		while(i<=100)
-		{
-			System.out.println(i);
-			i++;
-		}
+		int a[]= {10,20,30,40,50};	
+		//for integer array
+				for(int i:a)
+				{
+				System.out.println(i);	
+				}
+				
+				char ch[]= {'j','a','v','a'};
+				for(char c:ch)
+				{
+					System.out.print(c);		
+				}
+				System.out.println(" ");
+				String s1[]= {"Java","programming","learning"};
+		//for String array
+				for(String s:s1)
+				{
+					System.out.print(s+" ");		
+				}
 
 	}
 }
